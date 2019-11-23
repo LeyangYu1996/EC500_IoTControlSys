44f4163d5b3336ef747d4d0d059d41678d1db543
LeyangYu1996 <yly@bu.edu>
2019-11-23 17:15:04 -0500

    Update with data optimization

---
+++ optimizeDataTransfer/client.cpp
@@ -0,0 +1,158 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <string>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
+#include <time.h>       /* time_t, struct tm, difftime, time, mktime */
+#include <chrono>
+#include <thread>
+#include <sstream>
+#include <fcntl.h>
+#include <linux/i2c-dev.h>
+#include <errno.h>
+#include <cstring>
+#include <sys/ioctl.h>
+#define I2C_ADDR 0x23
+
+using namespace std::chrono;
+
+int interval = 100;
+
+void error(const char *msg)
+{
+    perror(msg);
+    exit(0);
+}
+float read_func();
+int main(int argc, char *argv[])
+{
+    int sockfd, portno, n;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+
+    char buffer[256];
+    if (argc < 3) {
+       fprintf(stderr,"usage %s hostname port\n", argv[0]);
+       exit(0);
+    }
+    portno = atoi(argv[2]);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) 
+        error("ERROR opening socket");
+    server = gethostbyname(argv[1]);
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr, 
+         (char *)&serv_addr.sin_addr.s_addr,
+         server->h_length);
+    serv_addr.sin_port = htons(portno);
+    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+        error("ERROR connecting");
+        
+        
+    bzero(buffer,256);
+    
+    /* SYNCING */
+    std::ostringstream oss;
+    n = read(sockfd,buffer,255);
+    if (n < 0) 
+        error("ERROR reading from socket");
+    
+    milliseconds ms = duration_cast< milliseconds >(
+        system_clock::now().time_since_epoch());
+
+    bzero(buffer,256);
+    
+    strcpy(buffer, std::to_string(ms.count()).c_str());
+    
+    n = write(sockfd,buffer,strlen(buffer));
+            
+    if (n < 0) 
+        error("ERROR writing to socket");
+
+    while(1){
+        bzero(buffer,256);
+        auto x = std::chrono::steady_clock::now() + std::chrono::milliseconds(interval);
+
+        time_t my_time = time(NULL); 
+
+        char lum[10];
+        snprintf(lum, 10, "%f", read_func());
+        
+        strcpy(buffer, lum);
+        
+        //strcat(buffer, " ");
+
+        std::this_thread::sleep_until(x);
+        
+        //milliseconds ms = duration_cast< milliseconds >(
+          //  system_clock::now().time_since_epoch());
+
+        //printf("%lld\n",ms.count());
+        
+        //strcat(buffer, std::to_string(ms.count()).c_str());
+        
+        while(strcmp(buffer,"ACK")!=0){
+            
+            n = write(sockfd,buffer,strlen(buffer));
+            
+            if (n < 0) 
+                error("ERROR writing to socket");
+            bzero(buffer,256);
+            n = read(sockfd,buffer,255);
+            if (n < 0) 
+                error("ERROR reading from socket");
+        }
+        
+        oss.str("");
+        oss.clear();
+        
+     }
+
+        
+    close(sockfd);
+    return 0;
+}
+
+float read_func(){
+    int fd;
+    char buf[3];
+    char val,value;
+    float flight;
+    fd=open("/dev/i2c-1",O_RDWR);
+    if(fd<0)
+    {
+        printf("Error reading from file:%s\r\n",strerror(errno)); return 1;
+    }
+    if(ioctl( fd,I2C_SLAVE,I2C_ADDR)<0 )
+    {
+        printf("ioctl ERR: %s\r\n",strerror(errno));return 1;
+    }
+    val=0x01;
+    if(write(fd,&val,1)<0)
+    {
+        printf("Error to power up\r\n");
+    }
+    val=0x11;
+    if(write(fd,&val,1)<0)
+    {
+        printf("Starting the High-Resolution mode2\r\n");
+    }
+    usleep(200000);
+    if(read(fd,&buf,3)){
+        flight=(buf[0]*256+buf[1])*0.5/1.2;
+        //printf("Luminum: %6.2flx\r\n",flight);
+    }
+    else{
+        printf("Reading Error\r\n");
+    }
+    return flight;
+}
---
+++ optimizeDataTransfer/server.cpp
@@ -0,0 +1,113 @@
+/* A simple server in the internet domain using TCP
+   The port number is passed as an argument */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
+#include <time.h>       /* time_t, struct tm, difftime, time, mktime */
+#include <chrono>
+#include <thread>
+#include <sstream>
+#include <fcntl.h>
+#include <linux/i2c-dev.h>
+#include <errno.h>
+#include <cstring>
+#include <sys/ioctl.h>
+using namespace std::chrono;
+
+void error(const char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+int main(int argc, char *argv[])
+{
+     int sockfd, newsockfd, portno;
+     socklen_t clilen;
+     char buffer[256];
+     struct sockaddr_in serv_addr, cli_addr;
+     int n;
+     if (argc < 2) {
+         fprintf(stderr,"ERROR, no port provided\n");
+         exit(1);
+     }
+     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+     if (sockfd < 0) 
+        error("ERROR opening socket");
+     bzero((char *) &serv_addr, sizeof(serv_addr));
+     portno = atoi(argv[1]);
+     serv_addr.sin_family = AF_INET;
+     serv_addr.sin_addr.s_addr = INADDR_ANY;
+     serv_addr.sin_port = htons(portno);
+     if (bind(sockfd, (struct sockaddr *) &serv_addr,
+              sizeof(serv_addr)) < 0) 
+              error("ERROR on binding");
+     listen(sockfd,5);
+     clilen = sizeof(cli_addr);
+     newsockfd = accept(sockfd, 
+                 (struct sockaddr *) &cli_addr, 
+                 &clilen);
+     if (newsockfd < 0) 
+          error("ERROR on accept");
+     bzero(buffer,256);
+
+     /*   SYNC PART    */   
+     printf("Syncing\n");
+     milliseconds ts1 = duration_cast< milliseconds >(
+            system_clock::now().time_since_epoch());
+
+     printf("Now time: %lld\n",ts1.count());
+
+        
+     strcpy(buffer, std::to_string(ts1.count()).c_str());
+     
+     long long int t1 = ts1.count();
+     
+     n = write(newsockfd,buffer,255);
+     if (n < 0) error("ERROR writing to socket");
+     
+     n = read(newsockfd,buffer,255);
+     if (n < 0) error("ERROR reading from socket");
+
+     milliseconds ts4 = duration_cast< milliseconds >(
+            system_clock::now().time_since_epoch());
+            
+     long long int t4 = ts4.count();
+            
+     long long int t2 = atoll(buffer);
+
+     long long int terr = t2 - ( t1 + (t4 - t1) / 2);
+     
+     printf("Time Error is %d\n", terr);
+     
+     long long int time_after_sync;
+     
+     float lum;
+
+     while(1){
+         bzero(buffer,256);
+         n = read(newsockfd,buffer,255);
+         milliseconds ms = duration_cast< milliseconds >(
+               system_clock::now().time_since_epoch());
+               
+         time_after_sync = ms.count() - terr;
+         if (n < 0) error("ERROR reading from socket");
+         printf("Here is the message: %s\ntime is %lld\ntime without sync is %lld\n",buffer, time_after_sync, ms.count());
+         
+         lum = std::stof(buffer);
+         
+         printf("%f", lum);
+         
+         n = write(newsockfd,"ACK",3);
+         if (n < 0) error("ERROR writing to socket");
+     }
+
+     close(newsockfd);
+     close(sockfd);
+     return 0; 
+}
